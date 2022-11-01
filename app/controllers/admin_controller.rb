adde91883640fac5155cd8b8eccb22f3ba51a2db
hungkieu <hungkieu.h12@gmail.com>
2022-11-01 19:51:22 +0700

    OOOO

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,15 @@ class AdminController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def delete
+    case params[:type]
+    when "post"
+      Post.find(params[:id]).destroy
+    when "category"
+      Category.find(params[:id]).destroy
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
 end
